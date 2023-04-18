a7251f5480599cc4b3e4b6daaa6e733f29e8e911
Anders Bo Pedersen <ABP@ao.dk>
2023-04-18 10:12:26 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿namespace tråd_synkronisering_opg2og3
+{
+    internal class Program
+    {
+        static int forForloops = 60;
+
+        static object _lock = new object();
+
+        static void Main(string[] args)
+        {
+            Thread s = new Thread(star);
+            s.Start();
+
+            Thread h = new Thread(hash);
+            h.Start();
+
+            s.Join();
+            h.Join();
+
+        }
+
+        static void star()
+        {
+            while (true)
+            {
+                Monitor.Enter(_lock);
+                try
+                {
+                    for (int i = 0; i < forForloops; i++)
+                    {
+                        Console.Write('*');
+                    }
+                    Console.WriteLine(forForloops);
+                    forForloops += 60;
+                    Thread.Sleep(1000);
+                }
+                finally
+                {
+                    Monitor.Exit(_lock);
+                }
+            }
+
+        }
+
+        static void hash()
+        {
+            while (true)
+            {
+                Monitor.Enter(_lock);
+                try
+                {
+                    for (int i = 0; i < forForloops; i++)
+                    {
+                        Console.Write('#');
+                    }
+                    Console.WriteLine(forForloops);
+                    forForloops += 60;
+                    Thread.Sleep(1000);
+                }
+                finally
+                {
+                    Monitor.Exit(_lock);
+                }
+            }
+        }
+    }
+}
